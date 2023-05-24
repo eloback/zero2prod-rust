0ae973d723104ae40fcab32faa819cb3177eb656
Eduardo Loback <eduardolloback@gmail.com>
2023-05-24 11:04:48 -0300

    feat: tests with database validation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::net::TcpListener;
 
+use sqlx::PgPool;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
-
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    run(listener)?.await
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
