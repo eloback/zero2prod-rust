3511458f17eea5a953600d5c0054da26dab84a63
Eduardo Loback <eduardolloback@gmail.com>
2023-05-27 23:40:44 -0300

    feat: ssl and less logs in db.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::net::TcpListener;
 
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use zero2prod::{configuration::get_configuration, startup::run};
 
@@ -13,9 +12,9 @@ async fn main() -> Result<(), std::io::Error> {
 
     // Instanciate a connection pool with the configs of the server.
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
 
     let address = format!(
         "{}:{}",
