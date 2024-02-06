cd75f9db3f2c92319d434e5255f1a9f0eff0c26c
Eduardo Loback <eduardolloback@gmail.com>
2024-02-06 14:13:35 -0300

    feat: redis session storage

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -3,16 +3,16 @@ set -x
 set -eo pipefail
 
 if ! [ -x "$(command -v psql)" ]; then
-  echo >&2 "Error: psql is not installed."
-  exit 1
+	echo >&2 "Error: psql is not installed."
+	exit 1
 fi
 
 if ! [ -x "$(command -v sqlx)" ]; then
-  echo >&2 "Error: sqlx is not installed."
-  echo >&2 "Use:"
-  echo >&2 "    cargo install --version='~0.6' sqlx-cli --no-default-features --features rustls,postgres"
-  echo >&2 "to install it."
-  exit 1
+	echo >&2 "Error: sqlx is not installed."
+	echo >&2 "Use:"
+	echo >&2 "    cargo install --version='~0.6' sqlx-cli --no-default-features --features rustls,postgres"
+	echo >&2 "to install it."
+	exit 1
 fi
 
 # Check if a custom user has been set, otherwise default to 'postgres'
@@ -27,37 +27,36 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
 # Allow to skip Docker if a dockerized Postgres database is already running
-if [[ -z "${SKIP_DOCKER}" ]]
-then
-  # if a postgres container is running, print instructions to kill it and exit
-  RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
-  if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
-    echo >&2 "there is a postgres container already running, kill it with"
-    echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
-    exit 1
-  fi
-  # Launch postgres using Docker
-  docker run \
-      -e POSTGRES_USER=${DB_USER} \
-      -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-      -e POSTGRES_DB=${DB_NAME} \
-      -p "${DB_PORT}":5432 \
-      -d \
-      --name "postgres_$(date '+%s')" \
-      postgres -N 1000
-      # ^ Increased maximum number of connections for testing purposes
+if [[ -z "${SKIP_DOCKER}" ]]; then
+	# if a postgres container is running, print instructions to kill it and exit
+	RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+	if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+		echo >&2 "there is a postgres container already running, kill it with"
+		echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
+		exit 1
+	fi
+	# Launch postgres using Docker
+	docker run \
+		-e POSTGRES_USER=${DB_USER} \
+		-e POSTGRES_PASSWORD=${DB_PASSWORD} \
+		-e POSTGRES_DB=${DB_NAME} \
+		-p "${DB_PORT}":5432 \
+		-d \
+		--name "postgres_$(date '+%s')" \
+		postgres -N 1000
+	# ^ Increased maximum number of connections for testing purposes
 fi
 
 # Keep pinging Postgres until it's ready to accept commands
 until pg_isready --dbname "${DB_NAME}" --host "${DB_HOST}" --port "${DB_PORT}"; do
-  >&2 echo "Postgres is still unavailable - sleeping"
-  sleep 1
+	echo >&2 "Postgres is still unavailable - sleeping"
+	sleep 1
 done
 
->&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
+echo >&2 "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 sqlx database create
 sqlx migrate run
 
->&2 echo "Postgres has been migrated, ready to go!"
+echo >&2 "Postgres has been migrated, ready to go!"
