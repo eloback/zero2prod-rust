9f9fdeffdaac5514f7877990b99cac6ab3dc3a21
Eduardo Loback <eduardolloback@gmail.com>
2024-02-06 20:13:37 -0300

    refactor: typed session

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,5 +1,5 @@
-use actix_session::Session;
-use actix_web::{cookie::Cookie, error::InternalError, http::header::LOCATION, web, HttpResponse};
+use crate::session_state::TypedSession;
+use actix_web::{error::InternalError, http::header::LOCATION, web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::Secret;
 use sqlx::PgPool;
@@ -22,7 +22,7 @@ pub struct FormData {
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -34,7 +34,7 @@ pub async fn login(
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
             session.renew();
             session
-                .insert("user_id", user_id)
+                .insert_user_id(user_id)
                 .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
