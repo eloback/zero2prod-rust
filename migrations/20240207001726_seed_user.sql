eb5f378058f522543228ae3db58ab30012a9c688
Eduardo Loback <eduardolloback@gmail.com>
2024-02-07 00:58:51 -0300

    chore: seed user

---
+++ migrations/20240207001726_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+  '22eba915-3e63-4de9-a3f0-4b28d3b95aa1',
+  'admin',
+  '$argon2id$v=19$m=15000,t=2,p=1$dPZxgo92B9DnJG5HPsOT0A$5KFxzwmi+i8Py/BDjR59wV8kOI1HYVl3zqivOUYsCzE'
+);
